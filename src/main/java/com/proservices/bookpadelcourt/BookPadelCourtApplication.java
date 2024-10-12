6771d0cf18ba72f1786576c12eb7219ed471048f
aroubeidis <aroubeidis@gmail.com>
2024-10-12 14:06:29 +0300

    initial commit - pom.xml, application.yml, Application class etc

---
+++ src/main/java/com/proservices/bookpadelcourt/BookPadelCourtApplication.java
@@ -0,0 +1,13 @@
+package com.proservices.bookpadelcourt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPadelCourtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookPadelCourtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proservices/bookpadelcourt/BookPadelCourtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proservices.bookpadelcourt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookPadelCourtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
